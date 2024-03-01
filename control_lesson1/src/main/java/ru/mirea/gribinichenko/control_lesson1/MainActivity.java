bf4da6d6bee08ffdafc1314b2095c2c5e041bfb9
Glo_Murlik <gribinichenko.danil@yandex.ru>
2024-03-02 00:00:57 +0300

    initial commit

---
+++ control_lesson1/src/main/java/ru/mirea/gribinichenko/control_lesson1/MainActivity.java
@@ -0,0 +1,17 @@
+package ru.mirea.gribinichenko.control_lesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    //TextView tv = findViewById(R.id.editTextTextPersonName4);
+
+}
